1b1272a9aa334253d64fbe1b9cd84230571ac9b2
Dabao <cn41408@gmail.com>
2018-01-29 18:11:54 +0800

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# talkbear - 說說熊聊天室
+
+一個試著用 Socket.io , Node.js , php 打造的多人聊天室
---
+++ js/Cresize.js
@@ -0,0 +1,9 @@
+$(window).resize(function() {
+    // var bid_img_height = $("#main .bid_center_category div img:eq(0)").height();
+    // $("#main .bid_center_category div").css("height",bid_img_height);
+});
+
+window.onload = function(){
+    // var bid_img_height = $("#main .bid_center_category div img:eq(0)").height();
+    // $("#main .bid_center_category div").css("height",bid_img_height);
+}
