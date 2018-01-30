7838498f477b2e9abb992a6c27afb4c99dd2fd8a
Dabao <cn41408@gmail.com>
2018-01-30 18:19:41 +0800

    :zap: add socket.io module & server.js

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # talkbear - 說說熊聊天室
 
 一個試著用 Socket.io, Node.js, bootstrap 4, jQuery 打造的多人聊天室
+
+# Setup - Node.js
+yum install node
+
+# Setup - Socket.io
+npm install --save socket.io
+
+# START - server.js
+node server.js
--- servier.js
+++ servier.js
@@ -1,2 +1,63 @@
 // node server.js
 
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8001);
+
+function handler ( res, res ) {
+    // 要執行的 socket.io
+    fs.readFile(__dirname + '/chatroom.html'),
+    function ( err, data ) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading chatroom.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    } // end function 
+} // end function handler
+
+// LOG用
+function log(msg) {
+    var date = new DATE;
+    fs.appendFile(__dirname + "/log/msg.txt", date + msg + '\n', function(err){ if(err) { console.log(err) } }) ;
+} // end function  log_in
+
+io.on('connection' , function (socket) {
+
+    // io.emit: 廣播給所有包含Server本身的連線使用者
+    // socket.broadcast.emit: 廣播給除了Server本身以外的連線使用者
+
+    // NewUserIn: 使用者進房間
+    socket.on('userin',function(name) {
+        socket.username = name;
+        var address = socket.handshake.address;
+        console.log("New user " + name + " is logged. From " + address + ".");
+        io.emit('userin',{
+            username: socket.username
+        });
+        log();
+    }); // end socket.on function userin
+
+    // talk: 大廳發話廣播
+    socket.on('talk',function(data) {
+        // show on server
+        let msg = data[name] + ' : ' + data[msg];
+        console.log(msg);
+        io.emit('paste',"<div>"+msg+"</div>");
+        log(msg);
+    }); // end socket.on function talk
+
+    // exit: 使用者掰掰
+    socket.on('exit',function(name){
+        console.log(name + 'was quit.');
+        io.emit('paste',"<div>" + name + "依依不捨的離開惹 ... </div>");
+        log(name + " was quit.");
+    });
+
+
+
+}); // end io.on connection function
