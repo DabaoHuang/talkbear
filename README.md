7db7420e41504991571999b1b40fdb6245378d8d
Dabao <cn41408@gmail.com>
2018-02-02 16:58:41 +0800

    :tada: Ver.0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# talkbear - 說說熊聊天室 Ver.0.1
+# talkbear - 說說熊聊天室 Ver.0.2
 
 一個試著用 Socket.io, Node.js, bootstrap 4, jQuery 打造的多人聊天室
 
@@ -6,12 +6,27 @@
 yum install node
 
 ## Setup
-npm install --save express@4.15.2
-npm install --save socket.io
-npm install --save dateformat
+ - npm install --save express@4.15.2
+ - npm install --save socket.io
+ - npm install --save dateformat
+ - npm install --save prepend-file
 
 ## START - server.js
 node server.js
 
 ## Sign in
 http://locahost:3000
+
+
+### Ver.0.1
+ - 基本功能建置
+
+### Ver.0.2
+ - index.js : 'msg.txt' appendFile => preppendFile
+ - 多使用 npm 套件 prepend-file
+
+# Features
+ - 私密聊天
+ - 重新配色
+ - LOGO
+ - 進入聊天室後，自動載入前面的歷史訊息
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var dateformat = require('dateformat');
+var prependFile = require('prepend-file');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,13 +15,18 @@ http.listen(3000, function(){
 
 // LOG用
 function log(msg) {
-  fs.appendFile(__dirname + "/log/msg.txt", msg + '\n', function(err){ if(err) { console.log(err) } }) ;
+  prependFile(__dirname+"/log/msg.txt",msg + '\n', function(err){ if(err) { console.log(err) } }) ;
+  // fs.appendFile(__dirname + "/log/msg.txt", msg + '\n', function(err){ if(err) { console.log(err) } }) ;
 } // end function log_in
-
 
 io.on('connection' , function (socket) {
 
   console.log("A user connected.");
+
+  fs.readFile(__dirname+'/log/msg.txt','utf8',(err , data) => {
+    if ( err ) throw err;
+    socket.emit('paste',data);
+  });
 
   // io.emit: 廣播給所有包含Server本身的連線使用者
   // socket.broadcast.emit: 廣播給除了Server本身以外的連線使用者
@@ -40,9 +46,10 @@ io.on('connection' , function (socket) {
   // talk: 大廳發話廣播
   socket.on('talk',function(username,msg,msg_color) {
       var date = new Date;
+      let address = socket.handshake.address;      
       // show on server
       let output = "<div style='color:"+msg_color+"'>"+ dateformat(date,"yyyy, mm, dS h:MM:ss") + ' ' + username + ' : ' + msg +"</div>";
-      console.log(username);
+      console.log(dateformat(date,"yyyy, mm, dS h:MM:ss") + ' ' + username + ' : ' + msg + '  ' + address);
       io.emit('paste',output);
       log(output);
   }); // end socket.on function talk
@@ -54,7 +61,5 @@ io.on('connection' , function (socket) {
       io.emit('paste',"<div>" + name + "依依不捨的離開惹 ... </div>");
       log(name + " was quit.");
   });
-
-  // 待開發: 私敲
 
 }); // end io.on connection function
